321b758999cd85220f7639f8ee7a8aca9a1eb3cf
Alexander Holmgren <alexander.holmgren@icloud.com>
2022-04-28 12:34:16 +0200

    lets go

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-var player1;
+var player;
 
 function startGame() {
   myGameArea.start();
-  player1 = new component(30, 30, "red", 10, 120);
+  player = new component(30, 30, "red", 10, 120);
 }
 
 var myGameArea = {
   canvas: document.createElement("canvas"),
   start: function () {
+    this.canvas.width = 1260;
+    this.canvas.height = 584;
+    this.context = this.canvas.getContext("2d");
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear: function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
 };
 
@@ -21,4 +28,14 @@ function component(width, height, color, x, y) {
   ctx = myGameArea.context;
   ctx.fillStyle = color;
   ctx.fillRect(this.x, this.y, this.width, this.height);
+  this.update = function () {
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  };
+}
+
+function updateGameArea() {
+  myGameArea.clear();
+  player.update();
 }
